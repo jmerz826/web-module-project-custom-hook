dd0bc48ff63f722d7beef1db3d8087d1caf19f7b
John Merz <jmerz826@gmail.com>
2021-12-01 17:40:24 -0500

    complete

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import useLocalStorage from "./useLocalStorage";
 
 const useDarkMode = () => {
-    const [darkMode, setDarkMode] = useState(false);
+    // const [darkMode, setDarkMode] = useState(false);
+    const [darkMode, setDarkMode] = useLocalStorage('isDark', false)
 
     return [darkMode, setDarkMode];
 }
